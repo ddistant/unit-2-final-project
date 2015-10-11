7db5ecf1a7d12448771abbd62602206db679efdf
Justine Gartner <GartnerJustine@gmail.com>
2015-10-11 17:55:36 -0400

    added segue to VideoDetailViewController from InspirationViewController

--- finalProject/finalProject/InspirationViewController.m
+++ finalProject/finalProject/InspirationViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "InspirationViewController.h"
+#import "VideoDetailViewController.h"
 
 @interface InspirationViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@property (nonatomic) NSMutableArray *results;
 
 @end
 
@@ -23,6 +28,16 @@
     self.tableView.dataSource = self;
 }
 
+#pragma mark - Table View Delegate Methods
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (self.segmentedControl.selectedSegmentIndex == 1) {
+        
+        [self instantiateVideoDetailController];
+    }
+}
+
 
 #pragma mark - Table View Datasource Methods
 
@@ -33,19 +48,84 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    //return count of API results array
-    return 1;
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+        
+        //return count of Tutorials API Array
+        return 10;
+        
+    }else if (self.segmentedControl.selectedSegmentIndex == 1){
+        
+        //return the results of the Videos API Array
+        return 10;
+        
+    }else {
+        
+        //return count of MeetUps API results array
+        return 10;
+    }
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"InspirationCellIdentifier" forIndexPath:indexPath];
-    
-    //testing cell
-    cell.textLabel.text = @"testCellTitle";
-    cell.detailTextLabel.text = @"testCellDetail";
-    
-    return cell;
+    if (self.segmentedControl.selectedSegmentIndex == 0) {
+        
+        //replace with custom Tutorials Cell
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"InspirationCellIdentifier" forIndexPath:indexPath];
+        
+        //testing cell
+        cell.textLabel.text = @"testTutorialsCellTitle";
+        cell.detailTextLabel.text = @"testCellDetail";
+        
+        return cell;
+       
+        
+    }else if (self.segmentedControl.selectedSegmentIndex == 1){
+        
+        //replace with custom Videos cell
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"InspirationCellIdentifier" forIndexPath:indexPath];
+        
+        //testing cell
+        cell.textLabel.text = @"testVideosCellTitle";
+        cell.detailTextLabel.text = @"testCellDetail";
+        
+        return cell;
+        
+    }else {
+        
+        //replace with custom Meet-Ups cell
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"InspirationCellIdentifier" forIndexPath:indexPath];
+        
+        //testing cell
+        cell.textLabel.text = @"testMeet-UpsCellTitle";
+        cell.detailTextLabel.text = @"testCellDetail";
+        
+        return cell;
+    }
 }
+
+
+#pragma mark - segmented control
+
+- (IBAction)segmentedControlSelected:(UISegmentedControl *)sender {
+    
+    [self.tableView reloadData];
+    
+}
+
+
+#pragma mark - video detail view controller
+
+-(void)instantiateVideoDetailController{
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    VideoDetailViewController *videoDetailVC = [storyboard instantiateViewControllerWithIdentifier:@"VideoDetailViewController"];
+    
+    [self presentViewController:videoDetailVC animated:YES completion:nil];
+    
+    NSLog(@"did select row in Video segment");
+    
+}
+
 
 @end
